5737fabc12a7189c871a86552977e1b655040e50
FRANCISCO <francisco@FRANCISCOs-iMac.local>
2020-05-02 12:46:45 -0700

    fgfsgs

--- app.js
+++ app.js
@@ -1,18 +1,8 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1504\cocoasubrtf820
-{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
-{\colortbl;\red255\green255\blue255;\red27\green29\blue31;}
-{\*\expandedcolortbl;;\cssrgb\c14118\c15294\c16078;}
-\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\sl300\partightenfactor0
+var http = require('http');
+var fs = require('fs');
+var index = fs.readFileSync('index.html');
 
-\f0\fs26 \cf2 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec2 var http = require('http');\
-var fs = require('fs');\
-var index = fs.readFileSync('index.html');\
-\
-http.createServer(function (req, res) \{\
-  res.writeHead(200, \{'Content-Type': 'text/html\'92\});\
-  res.end(index);\
-\}).listen(process.env.PORT);\
-}
+http.createServer(function (req, res) {
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.end(index);
+}).listen(process.env.PORT);
